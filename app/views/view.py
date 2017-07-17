6a5fc512d5d3e608fa1c49ba119bce5327816c05
Mdoctor <master_51@163.com>
2017-07-17 16:26:10 +0800

    'add'

--- app/views/view.py
+++ app/views/view.py
@@ -5,16 +5,48 @@
 from flask import request, render_template, \
     flash, redirect, url_for, make_response
 from . import app
+from flask_login import login_required, current_user
+from flask_sqlalchemy import get_debug_queries
+from .forms import EditProfileForm, EditProfileAdminForm, PostForm,\
+    CommentForm
+from .. import db
+from ..models import Permission, Role, User, Post, Comment
+from ..decorators import admin_required, permission_required
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/python/')
 def index1():
     return render_template('index.html')
 
+
 @app.route('/ruby/')
 def index2():
     return render_template('index.html')
+
+
+@app.route('/post/<int:id>', methods=['GET', 'POST'])
+def post(id):
+    post = Post.query.get_or_404(id)
+    form = CommentForm()
+    if form.validate_on_submit():
+        comment = Comment(body=form.body.data,
+                          post=post,
+                          author=current_user._get_current_object())
+        db.session.add(comment)
+        flash('Your comment has been published.')
+        return redirect(url_for('.post', id=post.id, page=-1))
+    page = request.args.get('page', 1, type=int)
+    if page == -1:
+        page = (post.comments.count() - 1) // \
+            current_app.config['FLASKY_COMMENTS_PER_PAGE'] + 1
+    pagination = post.comments.order_by(Comment.timestamp.asc()).paginate(
+        page, per_page=current_app.config['FLASKY_COMMENTS_PER_PAGE'],
+        error_out=False)
+    comments = pagination.items
+    return render_template('post.html', posts=[post], form=form,
+                           comments=comments, pagination=pagination)
